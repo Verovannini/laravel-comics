ac2dc58805e8ab6741348d1bf5da60802149670a
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-06-08 16:55:40 +0200

    fine header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Comics</title>
 
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome and CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,48 @@
-header
+<header>
+    <!-- Start Header Top -->
+    <div class="header-top">
+        <div class="container">
+            <ul class="menu">
+                <li>Dc Power<sup>SM</sup> visa<i class="far fa-registered"></i></li>
+                <li>Additional dc sites <i class="fas fa-caret-down"></i></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End Header Top -->
+
+    <!-- Start Header Bottom -->
+    <div class="header-bottom">
+        <div class="container">
+            <!-- Start Logo -->
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo">
+            </div>
+            <!-- End Logo -->
+
+            <!-- Start Nav -->
+            <nav>
+                <ul class="menu">
+                    <li><a href="#">Characters</a></li>
+                    <li class="active"><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop <i class="fas fa-caret-down"></i></a></li>
+                    <li class="search"><input type="text" placeholder="Search"> <i class="fas fa-search"></i></li>
+                </ul>
+            </nav>
+            <!-- End Nav -->
+
+        </div>
+    </div>
+    <!-- End Header Bottom -->
+
+    <!-- Start Jumbotron -->
+    <div class="jumbotron"></div>
+    <!-- End Jumbotron -->
+
+</header>
