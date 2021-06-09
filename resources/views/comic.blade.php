56725205541924e18fbf5503e8dcfbed5c24f2e5
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-06-09 17:13:01 +0200

    end details page

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -58,7 +58,77 @@
         </div>
         <!-- End Comic Wrapper -->
 
-        
+        <!-- Start Comic Details -->
+        <div class="comic-details">
+            <div class="container-small">
+
+                <!-- Start Talent -->
+                <div class="talent">
+                    <h3>Talent</h3>
+
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="small-title">Art by:</div>
+                        <div class="list">
+                            @foreach($comic['artists'] as $artist) 
+                            {{$artist}},
+                            @endforeach
+                        </div>
+                    </div>
+                    <!-- End Row -->
+
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="small-title">Written by:</div>
+                        <div class="list">
+                            @foreach($comic['writers'] as $writer) 
+                            {{$writer}},
+                            @endforeach
+                        </div>
+                    </div>
+                    <!-- End Row -->
+
+                </div>
+                <!-- End Talent -->
+
+                <!-- Start Specs -->
+                <div class="specs">
+                    <h3>Specs</h3>
+
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="small-title">Series:</div>
+                        <div class="list">
+                            {{ $comic['type'] }}
+                        </div>
+                    </div>
+                    <!-- End Row -->
+
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="small-title">U.S. Price:</div>
+                        <div class="list">
+                            {{ $comic['price'] }}   
+                        </div>
+                    </div>
+                    <!-- End Row -->
+
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="small-title">On Sale Date:</div>
+                        <div class="list">
+                            {{ $comic['sale_date'] }}   
+                        </div>
+                    </div>
+                    <!-- End Row -->
+
+                </div>
+                <!-- End Specs -->
+
+            </div>
+            
+        </div>
+        <!-- End Comic Details -->
 
 
     </section>
